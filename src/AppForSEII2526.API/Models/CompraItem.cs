f6c5a5541df96105b5c58ed8426ed60eee80d08a
ADRIÁN VILLALGORDO NAVARRO <Adrian.Villalgordo@alu.uclm.es>
2025-10-02 19:14:49 +0200

    Creación de las clases herramienta, fabricante, compraItem.

---
+++ src/AppForSEII2526.API/Models/CompraItem.cs
@@ -0,0 +1,19 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class CompraItem
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad minima es 1")]
+        public int cantidad { get; set; }
+
+        [StringLength(200, ErrorMessage = "La descripcion no puede tener más de 200 caracteres.")]
+        public string descripcion { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(0.05, float.MaxValue, ErrorMessage = "El precio minimo es 0.05")]
+        [Precision(10, 2)]
+        public float precioTotal { get; set; }
+
+        public int herramientaId { get; set; }
+        public int compraId { get; set; }
+    }
+}
---
+++ src/AppForSEII2526.API/Models/Herramienta.cs
@@ -0,0 +1,27 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Herramienta
+    {
+        public int Id { get; set; }
+
+        [StringLength(50, ErrorMessage = "El nombre no puede tener más de 50 caracteres.")]
+        public string Nombre { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(0.05, float.MaxValue, ErrorMessage = "El precio minimo es 0.05")]
+        [Precision(10, 2)]
+        public float Precio { get; set; }
+
+
+        public string? TiempoReparacion { get; set; }
+
+        [StringLength(50, ErrorMessage = "El nombre del material no puede tener más de 50 caracteres.")]
+        public string Material { get; set; }
+
+        //public List<AlquilarItem> AlquilarItem { get; set; }
+        //public List<ReparacionItem> ItemsReparacion { get; set; }
+        //public List<OfertaItem> OfertaItems { get; set; }
+        public int FabricanteId { get; set; }
+        public List<ComprarItem> CompraItems { get; set; }
+    }
+}
