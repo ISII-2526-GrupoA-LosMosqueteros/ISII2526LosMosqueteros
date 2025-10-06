c6a7a164560ee14c24d7776ecac8b3398b428391
sdiaz23uclm <sergio.diaz23@alu.uclm.es>
2025-10-06 21:38:21 +0200

    Se han creado los atributos de los usuarios necesarios (#20)
    Se han creado los atributos de los usuarios necesarios

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -4,4 +4,12 @@ namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
+    [Display(Name = "Nombre")]
+    public string Name { get; set; }
+
+    [Display(Name = "Apellidos")]
+    public string Surname { get; set; }
+
+    [Display(Name = "Correo Electronico")]
+    public string Email { get; set; }
 }
