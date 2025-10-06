b7c305d823d6085bc238bb4eec2a8941d118d73d
adrian23757 <adrian.villalgordo@alu.uclm.es>
2025-10-06 07:48:04 +0200

    6 crear las clases del modelo para comprar herramientas (#13)
    * Creación de las clases herramienta, fabricante, compraItem.
    
    * Creación de clases Compra, CompraItem, Fabricante y Herramienta.
    
    * Correción de error en la definición del tipo de datos de los atributos relacionados con el precio.
    Correción de nombres de atributos.
    
    * Creación de la primary key compuesta de la clase CompraItem.

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,4 +5,10 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+
+    public DbSet<Fabricante> Fabricantes { get; set; }
+    public DbSet<Herramienta> Herramientas { get; set; }
+    public DbSet<Compra> Compra { get; set; }
+    public DbSet<CompraItem> CompraItems { get; set; }
+
 }
