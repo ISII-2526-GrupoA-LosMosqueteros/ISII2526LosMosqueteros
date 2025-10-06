2338790a6887dffbec0f84ae6d150695bee37a73
Martín <martin.belmonte@alu.uclm.es>
2025-10-06 21:05:06 +0200

    Creación de clases Oferta y OfertaItem.

---
+++ src/AppForSEII2526.API/Models/OfertaItem.cs
@@ -0,0 +1,22 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    [PrimaryKey(nameof(IdOferta), nameof(IdHerramienta))]
+    public class OfertaItem
+    {
+        public int IdHerramienta { get; set; }
+        public int IdOferta { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(0, 100, ErrorMessage = "El porcentaje debe estar entre 0 y 100")]
+        public int Porcentaje { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(0.05, float.MaxValue, ErrorMessage = "El precio minimo es 0.05")]
+        [Precision(10, 2)]
+        public decimal PrecioFinal { get; set; }
+
+        public Herramienta Herramienta { get; set; }
+        public Oferta Oferta { get; set; }
+
+    }
+}
