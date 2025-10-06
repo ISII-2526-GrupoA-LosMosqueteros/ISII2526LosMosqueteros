a7806c7f225bb464a43c95100e7cb22b64085931
sdiaz23uclm <sergio.diaz23@alu.uclm.es>
2025-10-06 20:43:34 +0200

    Clases Reparacion y ReparacionItem creadas con sus relaciones (#15)

---
+++ src/AppForSEII2526.API/Models/ReparacionItem.cs
@@ -0,0 +1,25 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    [PrimaryKey(nameof(IdReparacion), nameof(IdHerramienta))]
+    public class ReparacionItem
+    {
+        public int IdReparacion { get; set; }
+        public int IdHerramienta { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(0.5, 1000, ErrorMessage = "Minimo precio es 0.5 y el maximo 1000")]
+        [Display(Name = "Precio para reparar")]
+        [Precision(10, 2)]
+        public decimal Precio { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad minima es 1")]
+        public int Cantidad { get; set; }
+
+        [StringLength(200, ErrorMessage = "La descripcion no puede tener más de 200 caracteres.")]
+        public String? Descripcion { get; set; }
+
+        public Herramienta Herramienta { get; set; }
+
+        public Reparacion Reparacion { get; set; }
+    }
+}
