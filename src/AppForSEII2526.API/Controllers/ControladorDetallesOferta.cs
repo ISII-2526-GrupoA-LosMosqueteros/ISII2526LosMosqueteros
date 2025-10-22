a7495af064e53a0ef76919b9a35ac5724590afbb
MartinBelmonte <martin.belmonte@alu.uclm.es>
2025-10-22 16:58:52 +0200

    Creación del método GET para mostrar ofertas y sus correspondientes D… (#65)
    Creación del método GET para mostrar ofertas y sus correspondientes DTOs.

---
+++ src/AppForSEII2526.API/Controllers/ControladorDetallesOferta.cs
@@ -0,0 +1,66 @@
+﻿using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ControladorDetallesOferta : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<ControladorDetallesOferta> _logger;
+
+        public ControladorDetallesOferta(ApplicationDbContext context, ILogger<ControladorDetallesOferta> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<DetalleOfertaDTO>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetDetallesdeOfertasCreadas(int id)
+        {
+            if (_context.Compras == null)
+            {
+                _logger.LogError("No se encontraron compras en la base de datos.");
+                return NotFound();
+            }
+
+            var oferta = await _context.Ofertas
+                .Where(o => o.Id == id)
+                .Include(o => o.ApplicationUser)
+                .Include(o => o.OfertaItems)
+                    .ThenInclude(oi => oi.Herramienta)
+                        .ThenInclude(h => h.Fabricante)
+                .Select(o => new DetalleOfertaDTO(
+
+                    o.FechaInicio,
+                    o.FechaFinal,
+                    o.TiposMetodoPago.ToString(),
+                    o.TiposDirigdaOferta.ToString(),
+                    o.OfertaItems.Select(oi => new OfertaItemDTO(
+                        oi.Herramienta.Nombre,
+                        oi.Herramienta.Material,
+                        oi.Herramienta.Fabricante.Nombre,
+                        oi.Herramienta.Precio,
+                        oi.PrecioFinal,
+                        oi.Herramienta.Id
+                    )).ToList(),
+                    o.FechaOferta,
+                    o.Id
+                ))
+                .FirstOrDefaultAsync();
+
+            if (oferta == null)
+            {
+                _logger.LogError("No se encontraron detalles de oferta para el ID proporcionado: {Id}", id);
+                return NotFound();
+            }
+            return Ok(oferta);
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/DetalleOfertaDTO.cs
@@ -0,0 +1,38 @@
+﻿using AppForSEII2526.API.Models;
+
+namespace AppForSEII2526.API.DTOs
+{
+    public class DetalleOfertaDTO
+    {
+        public DetalleOfertaDTO(DateTime fechaInicio, DateTime fechaFinal, string tiposMetodoPago, string tiposDirigdaOferta, IList<OfertaItemDTO> ofertaItem, DateTime fechaOferta, int id)
+        {
+            FechaInicio = fechaInicio;
+            FechaFinal = fechaFinal;
+            TiposMetodoPago = tiposMetodoPago;
+            TiposDirigdaOferta = tiposDirigdaOferta;
+            OfertaItem = ofertaItem;
+            FechaOferta = fechaOferta;
+            Id = id;
+        }
+
+        public int Id { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime FechaInicio { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime FechaFinal { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime FechaOferta { get; set; }
+
+        public string TiposMetodoPago { get; set; }
+
+        public string TiposDirigdaOferta { get; set; }
+
+        public IList<OfertaItemDTO> OfertaItem { get; set; }
+    }
+}
