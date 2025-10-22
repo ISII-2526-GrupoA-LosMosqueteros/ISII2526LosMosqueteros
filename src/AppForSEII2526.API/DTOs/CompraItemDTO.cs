831cfeaaabf294d9b3cb79c25c52ad1420be3f2f
adrian23757 <adrian.villalgordo@alu.uclm.es>
2025-10-22 16:36:21 +0200

    Creación del método GET para mostrar detalles de la compra y su corre… (#62)
    Creación del método GET para mostrar detalles de la compra y su correspondiente DTO

---
+++ src/AppForSEII2526.API/DTOs/CompraItemDTO.cs
@@ -0,0 +1,34 @@
+﻿namespace AppForSEII2526.API.DTOs
+{
+    public class CompraItemDTO
+    {
+        public CompraItemDTO(int id, string nombre, string material, int cantidad, string descripcion, decimal precio)
+        {
+            Id = id;
+            Nombre = nombre;
+            Material = material;
+            Cantidad = cantidad;
+            Descripcion = descripcion;
+            Precio = precio;
+        }
+
+        public int Id { get; set; }
+        [StringLength(50, ErrorMessage = "El nombre no puede tener más de 50 caracteres.")]
+        public string Nombre { get; set; }
+
+        [StringLength(50, ErrorMessage = "El nombre del material no puede tener más de 50 caracteres.")]
+        public string Material { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad minima es 1")]
+        public int Cantidad { get; set; }
+
+        [StringLength(200, ErrorMessage = "La descripcion no puede tener más de 200 caracteres.")]
+        public string Descripcion { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(0.05, float.MaxValue, ErrorMessage = "El precio minimo es 0.05")]
+        [Precision(10, 2)]
+        public decimal Precio { get; set; }
+
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/DetallesCompraDTO.cs
@@ -0,0 +1,30 @@
+﻿namespace AppForSEII2526.API.DTOs
+{
+    public class DetallesCompraDTO
+    {
+        public DetallesCompraDTO(int id, string name, string surname, string? email, decimal precioTotal, DateTime fechaCompra, IList<CompraItemDTO> compraItem)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Email = email;
+            PrecioTotal = precioTotal;
+            FechaCompra = fechaCompra;
+            CompraItem = compraItem;
+        }
+
+        public int Id { get; set; }
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+
+        [Display(Name = "Apellidos")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Correo Electronico")]
+        public string? Email { get; set; }
+        public decimal PrecioTotal { get; set; }
+        public DateTime FechaCompra { get; set; }
+        public IList<CompraItemDTO> CompraItem { get; set; }
+
+    }
+}
