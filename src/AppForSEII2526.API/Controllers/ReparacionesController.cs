d2e7f193f23e08b06f544709cf41b6ab29597aee
sdiaz23uclm <sergio.diaz23@alu.uclm.es>
2025-10-22 12:56:37 +0200

    Creacion del metodo GET para mostrar reparaciones con su correspondie… (#63)
    Creacion del metodo GET para mostrar reparaciones con su correspondiente DTO
    
    Co-authored-by: Sergio <sergio.diaz23@uclm.es>

---
+++ src/AppForSEII2526.API/Controllers/ReparacionesController.cs
@@ -0,0 +1,65 @@
+﻿using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReparacionesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<ReparacionesController> _logger;
+
+        public ReparacionesController(ApplicationDbContext context, ILogger<ReparacionesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(DetalleRepararDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetDetalleReparar(int id)
+        {
+            if (_context.Reparaciones == null)
+            {
+                _logger.LogError("Error: Reparaciones table does not exist");
+                return NotFound();
+            }
+
+            var reparacion = await _context.Reparaciones
+             .Where(r => r.Id == id)
+                 .Include(r => r.ReparacionItems) //join table ReparacionItem
+                    .ThenInclude(ri => ri.Herramienta) //then join table Herramienta
+                        .ThenInclude(h => h.Fabricante) //then join table Fabricante
+             .Select(r => new DetalleRepararDTO(
+                 r.Id,
+                 r.FechaEntrega,
+                 r.FechaRecogida,
+                 r.PrecioTotal,
+                 r.ApplicationUser.Name,
+                 r.ApplicationUser.Surname,
+                 r.ReparacionItems
+                    .Select(ri => new RepararItemDTO(
+                        ri.Herramienta.Id,
+                        ri.Herramienta.Nombre,
+                        ri.Precio,
+                        ri.Descripcion,
+                        ri.Cantidad)
+                    ).ToList<RepararItemDTO>()))
+             .FirstOrDefaultAsync();
+
+
+            if (reparacion == null)
+            {
+                _logger.LogError($"Error: Rental with id {id} does not exist");
+                return NotFound();
+            }
+
+
+            return Ok(reparacion);
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/DetalleRepararDTO.cs
@@ -0,0 +1,44 @@
+﻿namespace AppForSEII2526.API.DTOs
+{
+    public class DetalleRepararDTO
+    {
+        public DetalleRepararDTO(int id, DateTime fechaEntrega, DateTime fechaRecogida,
+            decimal precioTotal, string name, string surname, IList<RepararItemDTO> repararItem)
+        {
+            Id = id;
+            FechaEntrega = fechaEntrega;
+            FechaRecogida = fechaRecogida;
+            PrecioTotal = precioTotal;
+            Name = name;
+            Surname = surname;
+            RepararItem = repararItem;
+        }
+
+        public int Id { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime FechaEntrega { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime FechaRecogida { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(0.5, 100, ErrorMessage = "Minimo es 0.5 y maximo 100")]
+        [Display(Name = "Precio Total de Alquiler")]
+        [Precision(10, 2)]
+        public decimal PrecioTotal { get; set; }
+
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+
+        [Display(Name = "Apellidos")]
+        public string Surname { get; set; }
+
+        IList<RepararItemDTO> RepararItem { get; set; }
+
+
+
+    }
+}
