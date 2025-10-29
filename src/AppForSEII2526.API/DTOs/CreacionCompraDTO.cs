cf461fd0c02d309fce501126d5a510dc4ae707a0
ADRIÁN VILLALGORDO NAVARRO <Adrian.Villalgordo@alu.uclm.es>
2025-10-29 12:42:25 +0100

    Creación del POST para la creación de compras

---
+++ src/AppForSEII2526.API/DTOs/CreacionCompraDTO.cs
@@ -0,0 +1,32 @@
+﻿namespace AppForSEII2526.API.DTOs
+{
+    public class CreacionCompraDTO
+    {
+        public CreacionCompraDTO(string name, string surname, string direccionEnvio, TiposMetodoPago tipoMetodoPago, string? phone, string? email, IList<CompraItemDTO> compraItems)
+        {
+            Name = name;
+            Surname = surname;
+            DireccionEnvio = direccionEnvio;
+            TipoMetodoPago = tipoMetodoPago;
+            Phone = phone;
+            Email = email;
+            CompraItems = compraItems;
+         
+        }
+
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+
+        [Display(Name = "Apellidos")]
+        public string Surname { get; set; }
+        public string DireccionEnvio { get; set; }
+        public TiposMetodoPago TipoMetodoPago { get; set; }
+
+        [Display(Name = "Telefono")]
+        public string? Phone { get; set; }
+        [Display(Name = "Correo Electronico")]
+        public string? Email { get; set; }
+        public IList<CompraItemDTO> CompraItems { get; set; }
+
+    }
+}
