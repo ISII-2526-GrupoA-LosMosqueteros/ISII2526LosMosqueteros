038edc06e5a30b2a0cf725e83530c0037d928ca2
Sergio <sergio.diaz23@uclm.es>
2025-11-16 22:00:29 +0100

    Cambios pedidos solucionados

--- src/AppForSEII2526.API/Data/BBDD.Fabricantes.data.sql
+++ src/AppForSEII2526.API/Data/BBDD.Fabricantes.data.sql
@@ -18,33 +18,20 @@ INSERT INTO [dbo].[AspNetUsers] ([Id], [Name], [Surname], [Phone], [UserName], [
 INSERT INTO [dbo].[AspNetUsers] ([Id], [Name], [Surname], [Phone], [UserName], [NormalizedUserName], [Email], [NormalizedEmail], [EmailConfirmed], [PasswordHash], [SecurityStamp], [ConcurrencyStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEnd], [LockoutEnabled], [AccessFailedCount]) VALUES (N'5', N'Javier', N'Lopez', 656789012, N'javier.lopez', N'JAVIER.LOPEZ', N'javier.lopez@example.com', N'JAVIER.LOPEZ@EXAMPLE.COM', 1, NULL, N'7286B66E-DF84-4C88-B617-CA366B16D997', N'6C2A4851-ACBF-47A3-8B65-3FB7609596C4', N'656789012', 1, 0, NULL, 0, 0)
 
 SET IDENTITY_INSERT [dbo].[Reparaciones] ON
-INSERT INTO [dbo].[Reparaciones] ([Id], [FechaEntrega], [FechaRecogida], [PrecioTotal], [TiposMetodoPago], [ApplicationUserId]) VALUES (1, N'2025-10-21 00:00:00', N'2025-10-22 00:00:00', 10.25, 1, 5)
+INSERT INTO [dbo].[Reparaciones] ([Id], [FechaEntrega], [FechaRecogida], [PrecioTotal], [TiposMetodoPago], [ApplicationUserId]) VALUES (1, N'2025-10-21 00:00:00', N'2025-10-22 00:00:00', 25.00, 1, 5)
 SET IDENTITY_INSERT [dbo].[Reparaciones] OFF
 
-INSERT INTO [dbo].[ReparacionItems] ([ReparacionId], [HerramientaId], [Precio], [Cantidad], [Descripcion]) VALUES (1, 1, 10.20, 2, N'Muy Bonito')
+INSERT INTO [dbo].[ReparacionItems] ([ReparacionId], [HerramientaId], [Precio], [Cantidad], [Descripcion]) VALUES (1, 1, 25.00, 2, N'Muy Bonito')
 
 SET IDENTITY_INSERT [dbo].[Alquileres] ON
 INSERT INTO [dbo].[Alquileres] ([Id], [DireccionEnvio], [FechaAlquiler], [FechaFin], [FechaInicio], [Periodo], [PrecioTotal], [TiposMetodoPago], [ApplicationUserId]) VALUES (1, N'Calle Mayor 12, Madrid', N'2025-10-14 00:00:00', N'2025-10-20 00:00:00', N'2025-10-15 00:00:00', 5, CAST(120.50 AS Decimal(10, 2)), 0, N'1')
 INSERT INTO [dbo].[Alquileres] ([Id], [DireccionEnvio], [FechaAlquiler], [FechaFin], [FechaInicio], [Periodo], [PrecioTotal], [TiposMetodoPago], [ApplicationUserId]) VALUES (2, N'Avenida Andalucía 45, Sevilla', N'2025-11-10 00:00:00', N'2025-11-17 00:00:00', N'2025-11-11 00:00:00', 6, CAST(98.75 AS Decimal(10, 2)), 1, N'2')
 INSERT INTO [dbo].[Alquileres] ([Id], [DireccionEnvio], [FechaAlquiler], [FechaFin], [FechaInicio], [Periodo], [PrecioTotal], [TiposMetodoPago], [ApplicationUserId]) VALUES (3, N'Calle del Carmen 33, Valencia', N'2025-11-25 00:00:00', N'2025-11-30 00:00:00', N'2025-11-26 00:00:00', 4, CAST(150.00 AS Decimal(10, 2)), 2, N'3')
-INSERT INTO [dbo].[Alquileres] ([Id], [DireccionEnvio], [FechaAlquiler], [FechaFin], [FechaInicio], [Periodo], [PrecioTotal], [TiposMetodoPago], [ApplicationUserId]) VALUES (6, N'Naranja 64', N'2025-10-29 11:44:06', N'0001-01-01 00:00:00', N'0001-01-01 00:00:00', 0, CAST(0.00 AS Decimal(10, 2)), 0, N'1')
-INSERT INTO [dbo].[Alquileres] ([Id], [DireccionEnvio], [FechaAlquiler], [FechaFin], [FechaInicio], [Periodo], [PrecioTotal], [TiposMetodoPago], [ApplicationUserId]) VALUES (7, N'Naranja 64', N'2025-10-29 11:47:19', N'0001-01-01 00:00:00', N'0001-01-01 00:00:00', 0, CAST(37.50 AS Decimal(10, 2)), 0, N'1')
-INSERT INTO [dbo].[Alquileres] ([Id], [DireccionEnvio], [FechaAlquiler], [FechaFin], [FechaInicio], [Periodo], [PrecioTotal], [TiposMetodoPago], [ApplicationUserId]) VALUES (8, N'string', N'2025-10-29 12:22:00', N'0001-01-01 00:00:00', N'0001-01-01 00:00:00', 0, CAST(0.00 AS Decimal(10, 2)), 0, N'1')
-INSERT INTO [dbo].[Alquileres] ([Id], [DireccionEnvio], [FechaAlquiler], [FechaFin], [FechaInicio], [Periodo], [PrecioTotal], [TiposMetodoPago], [ApplicationUserId]) VALUES (9, N'string', N'2025-10-29 12:23:01', N'0001-01-01 00:00:00', N'0001-01-01 00:00:00', 0, CAST(0.00 AS Decimal(10, 2)), 0, N'1')
-INSERT INTO [dbo].[Alquileres] ([Id], [DireccionEnvio], [FechaAlquiler], [FechaFin], [FechaInicio], [Periodo], [PrecioTotal], [TiposMetodoPago], [ApplicationUserId]) VALUES (10, N'string', N'2025-10-29 12:23:11', N'0001-01-01 00:00:00', N'0001-01-01 00:00:00', 0, CAST(0.00 AS Decimal(10, 2)), 0, N'1')
-INSERT INTO [dbo].[Alquileres] ([Id], [DireccionEnvio], [FechaAlquiler], [FechaFin], [FechaInicio], [Periodo], [PrecioTotal], [TiposMetodoPago], [ApplicationUserId]) VALUES (11, N'string', N'2025-10-29 12:24:29', N'0001-01-01 00:00:00', N'0001-01-01 00:00:00', 0, CAST(7.50 AS Decimal(10, 2)), 0, N'1')
-INSERT INTO [dbo].[Alquileres] ([Id], [DireccionEnvio], [FechaAlquiler], [FechaFin], [FechaInicio], [Periodo], [PrecioTotal], [TiposMetodoPago], [ApplicationUserId]) VALUES (12, N'string', N'2025-10-29 12:29:53', N'0001-01-01 00:00:00', N'0001-01-01 00:00:00', 0, CAST(15.00 AS Decimal(10, 2)), 0, N'1')
-INSERT INTO [dbo].[Alquileres] ([Id], [DireccionEnvio], [FechaAlquiler], [FechaFin], [FechaInicio], [Periodo], [PrecioTotal], [TiposMetodoPago], [ApplicationUserId]) VALUES (13, N'string', N'2025-10-29 12:30:37', N'0001-01-01 00:00:00', N'0001-01-01 00:00:00', 0, CAST(7.50 AS Decimal(10, 2)), 0, N'1')
 SET IDENTITY_INSERT [dbo].[Alquileres] OFF
 
-INSERT INTO [dbo].[AlquilarItems] ([AlquilerId], [HerramientaId], [Cantidad], [Precio]) VALUES (6, 1, 3, CAST(0.00 AS Decimal(10, 2)))
-INSERT INTO [dbo].[AlquilarItems] ([AlquilerId], [HerramientaId], [Cantidad], [Precio]) VALUES (7, 1, 3, CAST(37.50 AS Decimal(10, 2)))
-INSERT INTO [dbo].[AlquilarItems] ([AlquilerId], [HerramientaId], [Cantidad], [Precio]) VALUES (8, 1, 3, CAST(0.00 AS Decimal(10, 2)))
-INSERT INTO [dbo].[AlquilarItems] ([AlquilerId], [HerramientaId], [Cantidad], [Precio]) VALUES (9, 1, 3, CAST(0.00 AS Decimal(10, 2)))
-INSERT INTO [dbo].[AlquilarItems] ([AlquilerId], [HerramientaId], [Cantidad], [Precio]) VALUES (10, 1, 3, CAST(0.00 AS Decimal(10, 2)))
-INSERT INTO [dbo].[AlquilarItems] ([AlquilerId], [HerramientaId], [Cantidad], [Precio]) VALUES (11, 1, 3, CAST(7.50 AS Decimal(10, 2)))
-INSERT INTO [dbo].[AlquilarItems] ([AlquilerId], [HerramientaId], [Cantidad], [Precio]) VALUES (12, 1, 3, CAST(15.00 AS Decimal(10, 2)))
-INSERT INTO [dbo].[AlquilarItems] ([AlquilerId], [HerramientaId], [Cantidad], [Precio]) VALUES (13, 1, 3, CAST(7.50 AS Decimal(10, 2)))
+INSERT INTO [dbo].[AlquilarItems] ([AlquilerId], [HerramientaId], [Cantidad], [Precio]) VALUES (1, 1, 3, CAST(225.00 AS Decimal(10, 2)))
+INSERT INTO [dbo].[AlquilarItems] ([AlquilerId], [HerramientaId], [Cantidad], [Precio]) VALUES (2, 2, 3, CAST(216.30 AS Decimal(10, 2)))
+INSERT INTO [dbo].[AlquilarItems] ([AlquilerId], [HerramientaId], [Cantidad], [Precio]) VALUES (3, 1, 3, CAST(187.50 AS Decimal(10, 2)))
 
 SET IDENTITY_INSERT [dbo].[Compras] ON
 INSERT INTO [dbo].[Compras] ([Id], [DireccionEnvio], [FechaCompra], [PrecioTotal], [TipoMetodoPago], [ApplicationUserId]) VALUES (1, N'Calle Julian Gayarre', N'2025-04-12 00:00:00', CAST(125.00 AS Decimal(10, 2)), 2, NULL)
