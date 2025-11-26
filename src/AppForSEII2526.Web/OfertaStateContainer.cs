f03db79eddde7a36d838b598c21a6fbd5cc0306d
Martín <martin.belmonte@alu.uclm.es>
2025-11-26 11:47:44 +0100

    Container creado y program.cs modificado

---
+++ src/AppForSEII2526.Web/OfertaStateContainer.cs
@@ -0,0 +1,59 @@
+﻿using AppForSEII2526.Web.API;
+
+namespace AppForSEII2526.Web
+{
+    public class OfertaStateContainer
+    {
+        public CreacionOfertaDTO Oferta { get; private set; } = new CreacionOfertaDTO()
+        {
+            OfertaItem = new List<OfertaItemDTO>()
+        };
+
+        public decimal PrecioTotal
+        {
+            get
+            {
+                return (decimal)Oferta.OfertaItem.Sum(item => item.PrecioFinal);
+            }
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public void AñadirHerramientaParaOferta(HerramientasParaOfertaDTO herramienta)
+        {
+            if (!Oferta.OfertaItem.Any(oi => oi.Id == herramienta.Id))
+            {
+                decimal precioFinal = (decimal)herramienta.Precio * (1 - (Oferta.Porcentaje / 100m));
+
+                Oferta.OfertaItem.Add(new OfertaItemDTO()
+                {
+                    Id = herramienta.Id,
+                    Nombre = herramienta.Nombre,
+                    Material = herramienta.Material,
+                    Fabricante = herramienta.Fabricante,
+                    Precio = herramienta.Precio,
+                });
+            }
+        }
+
+        public void RemoveOfertaItemToRent(OfertaItemDTO item)
+        {
+            Oferta.OfertaItem.Remove(item);
+        }
+
+        public void ClearOfertaCart()
+        {
+            Oferta.OfertaItem.Clear();
+        }
+
+        public void OfertaProcessed()
+        {
+            Oferta = new CreacionOfertaDTO()
+            {
+                OfertaItem = new List<OfertaItemDTO>()
+            };
+        }
+    }
+}
--- src/AppForSEII2526.Web/Program.cs
+++ src/AppForSEII2526.Web/Program.cs
@@ -1,10 +1,11 @@
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using AppForSEII2526.Web;
+using AppForSEII2526.Web.API;
 using AppForSEII2526.Web.Components;
 using AppForSEII2526.Web.Components.Account;
 using AppForSEII2526.Web.Data;
-using AppForSEII2526.Web.API;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -42,7 +43,7 @@ string? URI2API = builder.Configuration.GetValue(typeof(string), "AppForHerramie
 //We creaete the service for accessing the API from where .WEB project
 builder.Services.AddScoped<AppForHerramientasAPIClient>(sp => new AppForHerramientasAPIClient(URI2API, new HttpClient()));
 
-
+builder.Services.AddScoped<OfertaStateContainer>();
 
 var app = builder.Build();
 
