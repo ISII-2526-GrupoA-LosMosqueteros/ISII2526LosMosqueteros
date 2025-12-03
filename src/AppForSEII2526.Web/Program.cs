02dbc4496109bc259f050e6a516a6188fc938c39
MarioTorres <127690550+MarioProgramacion@users.noreply.github.com>
2025-12-03 12:08:05 +0100

    Alquilar StateContainer y modificacion en Program creadas y o actualizadas (#130)
    Co-authored-by: sdiaz23uclm <sergio.diaz23@alu.uclm.es>

--- src/AppForSEII2526.Web/Program.cs
+++ src/AppForSEII2526.Web/Program.cs
@@ -43,6 +43,8 @@ string? URI2API = builder.Configuration.GetValue(typeof(string), "AppForHerramie
 //We creaete the service for accessing the API from where .WEB project
 builder.Services.AddScoped<AppForHerramientasAPIClient>(sp => new AppForHerramientasAPIClient(URI2API, new HttpClient()));
 
+builder.Services.AddScoped<AlquilarStateContainer>();
+
 builder.Services.AddScoped<ComprarStateContainer>();
 
 //adding an In-memory state container service
