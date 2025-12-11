abfac24df0fecfffe78a49b57ab1d76c2e141895
ADRIÁN VILLALGORDO NAVARRO <Adrian.Villalgordo@alu.uclm.es>
2025-12-11 12:48:53 +0100

    Implementacion de sistemas distribuidos

--- src/AppForSEII2526.API/Program.cs
+++ src/AppForSEII2526.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.Data.Sqlite;
 using System.Data.Common;
+using TodoApi.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -66,6 +67,8 @@ builder.Services.AddSwaggerGen(options => {
 
 });
 
+builder.Logging.AddRabbitMQ(builder.Configuration.GetSection("RabbitMQ"));
+//”RabbitMQ” coincide con el nombre del bloque de propiedades en appsettings.json 
 
 var app = builder.Build();
 
