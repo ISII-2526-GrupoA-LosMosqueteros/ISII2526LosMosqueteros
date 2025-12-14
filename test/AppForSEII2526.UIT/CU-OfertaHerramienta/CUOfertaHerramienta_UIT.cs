b6f1d1a840f92aa963d9a7f114dbc5c907edd751
Martín <martin.belmonte@alu.uclm.es>
2025-12-14 12:43:21 +0100

    commit para traer cambios de development

---
+++ test/AppForSEII2526.UIT/CU-OfertaHerramienta/CUOfertaHerramienta_UIT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.CU_OfertaHerramienta
+{
+    public class CUOfertaHerramienta_UIT : UC_UIT
+    {
+        public CUOfertaHerramienta_UIT(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
